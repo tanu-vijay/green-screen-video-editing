06f5942ff1a5160e1de40ab8dc693bbe007e7339
tanu-vijay <98703062+tanu-vijay@users.noreply.github.com>
2022-07-21 18:01:04 +0530

    Add files via upload

---
+++ proj.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+
+video = cv2.VideoCapture("video.mp4")
+image = cv2.imread("img.jpg")
+
+while True:
+    ret, frame = video.read() 
+    frame=cv2.resize(frame, (640, 480))
+    image=cv2.resize(image, (640, 480))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_g=np.array([32, 94, 132])
+    u_g=np.array([179, 255, 255])
+
+    mask = cv2.inRange(hsv, l_g, u_g)
+    res=cv2.bitwise_and(frame, frame, mask=mask)
+    f=frame-res
+    green_screen=np.where(f==0, image, f)
+    cv2.imshow("final", green_screen)
+    
+    k = cv2.waitKey(1) 
+    if k==ord('q'):  
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+
